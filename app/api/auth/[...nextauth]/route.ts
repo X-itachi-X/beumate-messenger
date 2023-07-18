5506a6225061fa9ff98985994a525a59449855da
Ankit Prakash <104882734+X-itachi-X@users.noreply.github.com>
2023-07-18 15:30:21 +0000

    Jul 18, 2023, 9:00 PM

--- app/api/auth/[...nextauth]/route.ts
+++ app/api/auth/[...nextauth]/route.ts
@@ -6,3 +6,7 @@ import GoogleProvider from "next-auth/providers/google";
 import { PrismaAdapter } from "@next-auth/prisma-adapter";
 
 import prisma from "@/app/libs/prismadb";
+
+export const authOptions: AuthOptions = {
+    adapter: PrismaAdapter(prisma)
+}

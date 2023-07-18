b0f9a1619cf52c02ddd13aa7c01a721d802456aa
Ankit Prakash <104882734+X-itachi-X@users.noreply.github.com>
2023-07-18 05:36:02 +0530

    modified:   .vscode/settings.json 	modified:   app/page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,7 @@
-import Image from 'next/image'
-
 export default function Home() {
   return (
     <div>
-      <p>Hello Messenger</p>
+      <p className='text-sky-500 text-3xl'>Hello Messenger</p>
     </div>
   )
 }

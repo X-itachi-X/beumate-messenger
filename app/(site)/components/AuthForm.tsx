ef6355982835cad7cd2caf18c722f3421839ef70
Ankit Prakash <104882734+X-itachi-X@users.noreply.github.com>
2023-07-18 01:14:47 +0000

    Jul 18, 2023, 6:44 AM

--- app/(site)/components/AuthForm.tsx
+++ app/(site)/components/AuthForm.tsx
@@ -5,7 +5,7 @@ import { useState } from "react";
 type Variant = 'LOGIN' | 'REGISTER';
 
 const Authform = () => {
-    const [variant, setVariant] = useState();
+    const [variant, setVariant] = useState<Variant>('LOGIN');
     return (
         <div>Auth Form2!</div>
     );

562e2db1e94f166820a987c0725fbd7c2c2fc21e
Ankit Prakash <104882734+X-itachi-X@users.noreply.github.com>
2023-07-18 01:26:44 +0000

    Jul 18, 2023, 6:56 AM

--- app/(site)/components/AuthForm.tsx
+++ app/(site)/components/AuthForm.tsx
@@ -1,7 +1,10 @@
 'use client';
 
 import { useCallback, useState } from "react";
-import { FieldValues, useForm } from "react-hook-form";
+import { 
+    FieldValues, 
+    useForm 
+} from "react-hook-form";
 
 type Variant = 'LOGIN' | 'REGISTER';
 

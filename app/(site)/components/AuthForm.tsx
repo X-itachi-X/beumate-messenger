4f4134b278b6585c9d7c9138a91f577b2f990fbb
Ankit Prakash <104882734+X-itachi-X@users.noreply.github.com>
2023-07-18 11:32:21 +0000

    Jul 18, 2023, 5:02 PM

--- app/(site)/components/AuthForm.tsx
+++ app/(site)/components/AuthForm.tsx
@@ -1,6 +1,7 @@
 'use client';
 
 
+import Button from "@/app/components/Button";
 import Input from "@/app/components/inputs/Input";
 import { useCallback, useState } from "react";
 import { 
@@ -8,6 +9,8 @@ import {
     useForm,
     SubmitHandler 
 } from "react-hook-form";
+import AuthSocialButton from "./AuthSocialButton";
+import { BsGithub, BsGoogle } from 'react-icons/bs'
 
 type Variant = 'LOGIN' | 'REGISTER';
 
@@ -105,12 +108,76 @@ const Authform = () => {
                         register={register} 
                         errors={errors}
                         />
-                    
 
+                        <div>
+                            <Button
+                                disabled={isLoading}
+                                fullWidth
+                                type="submit"
+                            >
+                                {variant === 'LOGIN' ? 'SIGN IN' : 'REGISTER'}
+                            </Button>
+                        </div>
                 </form>
-            </div>
 
-        </div>
+                <div className="mt-6">
+                    <div className="relative">
+                        <div className="
+                                absolute
+                                inset-0
+                                flex
+                                items-center">
+                                                               
+                            <div 
+                            className="
+                            w-full border-t border-gray-300" 
+                            />
+                        </div>
+                            <div className="relitive flex justify-center text-sm">
+                                <span className="
+                                    bg-white
+                                    px-2
+                                    text-gray-500">
+                                        Or continue with
+                                </span>
+                            </div>
+                    </div>
+
+                    <div className="mt-6 flex gap-2">
+                        <AuthSocialButton 
+                            icon={BsGithub}
+                            onClick={() => socialAction('github')}
+                        />
+                        <AuthSocialButton 
+                            icon={BsGoogle}
+                            onClick={() => socialAction('google')}
+                        />
+                    </div>
+
+                </div>
+
+                <div className="
+                    flex
+                    gap-2
+                    justify-center
+                    text-sm
+                    mt-6
+                    px-2
+                    text-gray-500">
+                        <div>
+                            {variant === 'LOGIN' ? 'New User?' : 'Already have an account?'}
+                        </div>
+                        <div 
+                            onClick={toggleVariant}
+                            className="underline cursor-pointer"
+                        >
+                            {variant === 'LOGIN' ? 'Create an account' : 'Login'}
+                        </div>
+
+
+                </div>
+            </div>
+         </div>
     );
 }
 

9356d3a95daf5f8ee55c5445a026b1a75a5ea954
Ankit Prakash <104882734+X-itachi-X@users.noreply.github.com>
2023-07-18 00:24:34 +0000

    Jul 18, 2023, 5:54 AM

--- app/(site)/page.tsx
+++ app/(site)/page.tsx
@@ -1,7 +1,16 @@
 export default function Home() {
     return (
-      <div>
-        <p className='text-sky-500 text-3xl'>Hello Index page</p>
+      <div
+        className="
+          flex
+          min-h-full
+          flex-col
+          justify-center
+          py-12
+          sm:px-6
+          lg:px-8
+          bg-gray-100">
+       
       </div>
     )
   }

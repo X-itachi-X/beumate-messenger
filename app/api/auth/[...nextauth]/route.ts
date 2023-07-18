942d6d459dad096985783e56c605f37c6a2db43d
Ankit Prakash <104882734+X-itachi-X@users.noreply.github.com>
2023-07-18 15:26:41 +0000

    Jul 18, 2023, 8:56 PM

--- app/api/auth/[...nextauth]/route.ts
+++ app/api/auth/[...nextauth]/route.ts
@@ -3,3 +3,4 @@ import NextAuth, { AuthOptions } from "next-auth";
 import { CredentialsProvider } from "next-auth/providers/credentials";
 import GithubProvider from "next-auth/providers/github";
 import GoogleProvider from "next-auth/providers/google";
+import { PrismaAdapter } from "@next-auth/prisma-adapter";

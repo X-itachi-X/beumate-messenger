8f651de7e6856a08cd0c67bb2f6ac9978c599181
Ankit Prakash <104882734+X-itachi-X@users.noreply.github.com>
2023-07-18 00:48:02 +0000

    Jul 18, 2023, 6:18 AM

--- app/(site)/page.tsx
+++ app/(site)/page.tsx
@@ -1,3 +1,4 @@
+import Image from "next/image";
 export default function Home() {
     return (
       <div
@@ -12,7 +13,13 @@ export default function Home() {
           bg-gray-100">
 
             <div className="sm:mx-auto sm:w-full sm:max-w-md">
-              
+              <Image
+                alt="Logo"
+                height="48"
+                width="48"
+                className="mx-auto w-auto"
+                src="/image/logo.png"
+                />
             </div>
        
       </div>

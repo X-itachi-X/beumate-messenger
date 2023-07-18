3211dba2a8b1a1fd67a46facf74bc1444b238e9d
Ankit Prakash <104882734+X-itachi-X@users.noreply.github.com>
2023-07-18 00:53:12 +0000

    Jul 18, 2023, 6:23 AM

--- app/(site)/page.tsx
+++ app/(site)/page.tsx
@@ -20,6 +20,19 @@ export default function Home() {
                 className="mx-auto w-auto"
                 src="/images/logo.png"
                 />
+              
+              <h2
+                className="
+                  mt-6
+                  text-center
+                  text-3xl
+                  font-bold
+                  tracking-tight
+                  text-gray-900             
+                "
+              >
+                Sign in to your account
+              </h2>  
             </div>
        
       </div>

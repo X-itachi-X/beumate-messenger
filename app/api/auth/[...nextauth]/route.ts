3353a612953a7667f3a15644a598757b2c7a939f
Ankit Prakash <104882734+X-itachi-X@users.noreply.github.com>
2023-07-18 15:26:16 +0000

    Jul 18, 2023, 8:56 PM

--- app/api/auth/[...nextauth]/route.ts
+++ app/api/auth/[...nextauth]/route.ts
@@ -1,4 +1,5 @@
 import bcrypt from "bcrypt";
 import NextAuth, { AuthOptions } from "next-auth";
 import { CredentialsProvider } from "next-auth/providers/credentials";
-import GithubProvider from "next-auth/providers/github"
+import GithubProvider from "next-auth/providers/github";
+import GoogleProvider from "next-auth/providers/google";

9b6e71165fdbd7f281a8c19a9d507aba355ad0e4
Ankit Prakash <104882734+X-itachi-X@users.noreply.github.com>
2023-07-19 15:32:42 +0000

    Jul 19, 2023, 9:02 PM

---
+++ app/api/register/route.ts
@@ -0,0 +1,10 @@
+import bcrypt from "bcrypt";
+
+import prisma from "@/app/libs/prismadb";
+import { NextResponse } from "next/server";
+
+export async function POST(
+    request: Request
+){
+    
+}

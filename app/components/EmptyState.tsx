094fd017f742308dd3506b3cfb6370924f82a319
Ankit Prakash <104882734+X-itachi-X@users.noreply.github.com>
2023-08-25 06:41:02 +0000

    Aug 25, 2023, 12:11 PM

--- app/components/EmptyState.tsx
+++ app/components/EmptyState.tsx
@@ -16,7 +16,7 @@ const EmptyState = () => {
       >
         <div className="text-center items-center flex flex-col">
           <h3 className="mt-2 text-2xl font-semibold text-gray-900">
-            Select a chat or start a new conversation
+            Say Hello to Someone 👋
           </h3>
         </div>
       </div>

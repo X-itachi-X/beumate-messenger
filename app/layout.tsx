7c0e6527989e1d597c3779ba292326258638c3f8
Ankit Prakash <104882734+X-itachi-X@users.noreply.github.com>
2023-08-25 04:17:32 +0000

    Aug 25, 2023, 9:47 AM

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,5 +1,6 @@
 import './globals.css'
 import type { Metadata } from 'next'
+import ToasterContext from './context/ToasterContext'
 import { Inter } from 'next/font/google'
 
 const inter = Inter({ subsets: ['latin'] })
@@ -16,7 +17,9 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
-      <body className={inter.className}>{children}</body>
+      <body className={inter.className}>
+        <ToasterContext />
+        {children}</body>
     </html>
   )
 }

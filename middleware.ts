7f4413f95a6e6f8ddc32fc137b1d2831d7773c10
Ankit Prakash <104882734+X-itachi-X@users.noreply.github.com>
2023-08-25 06:31:04 +0000

    Aug 25, 2023, 12:01 PM

--- middleware.ts
+++ middleware.ts
@@ -1,1 +1,14 @@
-import 
+import { withAuth } from "next-auth/middleware";
+
+export default withAuth({
+  pages: {
+    signIn: "/",
+  },
+});
+
+export const config = { 
+  matcher: [
+    "/conversations/:path*",
+    "/users/:path*",
+  ]
+};

a60190498405fa430f587d574cbcb7ad67f537fe
Ankit Prakash <104882734+X-itachi-X@users.noreply.github.com>
2023-08-25 07:54:01 +0000

    Aug 25, 2023, 1:24 PM

--- app/(site)/page.tsx
+++ app/(site)/page.tsx
@@ -1,42 +1,44 @@
 import Image from "next/image";
-import Authform from "./components/AuthForm";
-export default function Home() {
-    return (
-      <div
-        className="
-          flex
-          min-h-full
-          flex-col
-          justify-center
-          py-12
-          sm:px-6
-          lg:px-8
-          bg-gray-100">
+import AuthForm from "./components/AuthForm";
 
-            <div className="sm:mx-auto sm:w-full sm:max-w-md">
-              <Image
-                alt="Logo"
-                height="48"
-                width="48"
-                className="mx-auto w-auto"
-                src="/images/logo.png"
-                />
-              
-              <h2
-                className="
-                  mt-6
-                  text-center
-                  text-3xl
-                  font-bold
-                  tracking-tight
-                  text-gray-900             
-                "
-              >
-                Sign in to your account
-              </h2>  
-            </div>
-          <Authform/>
+const Auth = () => {
+  return (
+    <div 
+      className="
+        flex 
+        min-h-full 
+        flex-col 
+        justify-center 
+        py-12 
+        sm:px-6 
+        lg:px-8 
+        bg-gray-100
+      "
+    >
+      <div className="sm:mx-auto sm:w-full sm:max-w-md">
+        <Image
+          height="48"
+          width="48"
+          className="mx-auto w-auto"
+          src="/images/logo.png"
+          alt="Logo"
+        />
+        <h2 
+          className="
+            mt-6 
+            text-center 
+            text-3xl 
+            font-bold 
+            tracking-tight 
+            text-gray-900
+          "
+          >
+            Sign in to your account
+        </h2>
       </div>
-    )
-  }
-  
+      <AuthForm />      
+  </div>
+  )
+}
+
+export default Auth;

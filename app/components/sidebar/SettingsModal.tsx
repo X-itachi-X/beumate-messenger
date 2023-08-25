fe2edff3fc2d298f67ee146f153e25cf88253ea9
Ankit Prakash <104882734+X-itachi-X@users.noreply.github.com>
2023-08-25 08:44:31 +0000

    Aug 25, 2023, 2:14 PM

--- app/components/sidebar/SettingsModal.tsx
+++ app/components/sidebar/SettingsModal.tsx
@@ -116,7 +116,7 @@ const SettingsModal: React.FC<SettingsModalProps> = ({
                   <CldUploadButton 
                     options={{ maxFiles: 1 }} 
                     onUpload={handleUpload} 
-                    uploadPreset="pgc9ehd5"
+                    uploadPreset="iipylss1"
                   >
                     <Button
                       disabled={isLoading}

88e96ed6a72ccbbb4f3039d527bbc8d8c2e05fb0
Ankit Prakash <104882734+X-itachi-X@users.noreply.github.com>
2023-08-25 06:12:21 +0000

    Aug 25, 2023, 11:42 AM

--- app/(site)/components/AuthForm.tsx
+++ app/(site)/components/AuthForm.tsx
@@ -4,7 +4,7 @@
 import Button from "@/app/components/Button";
 import axios from "axios";
 import Input from "@/app/components/inputs/Input";
-import { useCallback, useState } from "react";
+import { useCallback, useEffect, useState } from "react";
 import { 
     FieldValues, 
     useForm,
@@ -13,15 +13,22 @@ import {
 import AuthSocialButton from "./AuthSocialButton";
 import { BsGithub, BsGoogle } from 'react-icons/bs'
 import toast from "react-hot-toast";
-import { signIn} from "next-auth/react";
-import { callbackify } from "util";
+import { signIn, useSession} from "next-auth/react";
+
 
 
 type Variant = 'LOGIN' | 'REGISTER';
 
 const Authform = () => {
+    const session = useSession();
     const [variant, setVariant] = useState<Variant>('LOGIN');
     const [isLoading, setIsLoading] = useState(false);
+
+    useEffect(() => {
+        if (session?.status == 'authenticated'){
+            console.log('Authenticated')
+        }
+    }, [session?.status]);
 
     const toggleVariant = useCallback(() => {
         if (variant === 'LOGIN'){
